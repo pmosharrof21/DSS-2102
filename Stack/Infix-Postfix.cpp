130612f38af9f0e82647851a1b75c0e6d15e4f21
pmosharrof21 <pmosharrof3103@gmail.com>
2025-01-22 00:56:50 +0600

    Add files via upload

---
+++ Stack/Infix-Postfix.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int precedence(char op) {
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    return 0;
+}
+string infixToPostfix(string infix) {
+    stack<char> s;
+    string postfix;
+    for (char ch : infix) {
+        if (isalnum(ch)) {
+            postfix += ch;
+        } else if (ch == '(') {
+            s.push(ch);
+        } else if (ch == ')') {
+            while (!s.empty() && s.top() != '(') {
+                postfix += s.top();
+                s.pop();
+            }
+            s.pop();
+        } else {
+            while (!s.empty() && precedence(s.top()) >= precedence(ch)) {
+                postfix += s.top();
+                s.pop();
+            }
+            s.push(ch);
+        }
+    }
+    while (!s.empty()) {
+        postfix += s.top();
+        s.pop();
+    }
+    return postfix;
+}
+int main() {
+    string infix = "a+b*(c^d-e)^(f+g*h)-i";
+    cout << "Infix Expression: " << infix << endl;
+    cout << "Postfix Expression: " << infixToPostfix(infix) << endl;
+    return 0;
+}
