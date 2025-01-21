aeea71abeb104e4e9611c396bec7c6ef740e8986
pmosharrof21 <pmosharrof3103@gmail.com>
2025-01-22 00:28:21 +0600

    Add files via upload

---
+++ Sorting/InsertionS.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {1, 6, 9, 3, 12};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, size);
+    printArray(arr, size);
+    return 0;
+}
+
